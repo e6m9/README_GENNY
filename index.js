80803d1c6e03777197c7527ab3aa1954f3865831
e6m9 <alostsound@gmail.com>
2023-12-28 21:00:17 -0500

    added modules and questions

---
+++ index.js
@@ -0,0 +1,26 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = ['what is the title of yr project?',
+'please enter a description of yr project...',
+'please enter a table of contents for yr project...',
+'please describe the installation of yr project...',
+'please describe the usage of yr project...',
+'please enter the type of license for yr project from the following options...',
+'please name the contributors to yr project...',
+'please enter the test instructions for yr project...',
+'please enter yr contact information...'];
+
+// TODO: Create a function to write README file
+const writeToFile = (fileName, data) => {
+
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+const init = 
+
+// Function call to initialize app
+init();
